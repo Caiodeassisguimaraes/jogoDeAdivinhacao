02cfc81832c9145c08b1ff450814720c5e235a1b
Caio de Assis Guimarães <caiodeassisguimaraes@hotmail.com>
2023-05-27 16:03:56 -0300

    solution using object-oriented done, trying to improve more features

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,40 +1,23 @@
 package application;
 
-import java.util.Random;
 import java.util.Scanner;
 
+import service.GuessingGame;
+
 public class Main {
-    public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-    	Random random = new Random();
-    	int randomNumber = random.nextInt(101);    	
-        System.out.println("Bem-vindo ao jogo de adivinhação!");
-        System.out.println();
-        System.out.println("Tente adivinhar um número entre 1 e 100.");
-        System.out.println("Você tem 10 tentativas.");
-        System.out.println();
-        System.out.println(randomNumber);
-        int count = 0;
-        for (int i = 0; i < 10; i++) {
-        	count++;
-            System.out.print("Digite o seu palpite: ");
-            int guess = sc.nextInt();
-            
-			if (guess < randomNumber) {
-				System.out.println("O número é maior do que " + guess + ".");
-				
-			} else if (guess > randomNumber){
-				System.out.println("O número é menor do que " + guess + ".");
-			} if (guess == randomNumber){
-				System.out.println("Parabéns! Você acertou o número em " + count + " tentativas.");
-				break;
-			}
-			
-			System.out.println();
-        }
-                                
-        sc.close();
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		GuessingGame game = new GuessingGame(101, 10);
+		System.out.println(game.getRandomNumber());
+
+		//char answer;
+		//do {
+			game.startGame();
+		//	System.out.println("Quer jogar de novo? (s/n)");
+		//	answer = sc.next().charAt(0);
+		//} while (answer != 'n');
+
+		sc.close();
+	}
 }
-
-
--- src/service/GuessingGame.java
+++ src/service/GuessingGame.java
@@ -1,9 +1,109 @@
 package service;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessingGame {
 
-	//Esta Classe irá conter a lógica principal do negócio.
-	//Deve ser implementado um método para gerar um número aleatório,
-	//receber palpites do jogador e verificar se o palpite está correto.
-	
+	// Attributes
+
+	private int randomNumber;
+	private int maxRange;
+	private int maxTries;
+	private int tries;
+
+	// Constructors
+
+	public GuessingGame() {
+
+	}
+
+	public GuessingGame(int maxRange, int maxTries) {
+		this.maxRange = maxRange;
+		this.maxTries = maxTries;
+		this.setRandomNumber(createRandomNumber(maxRange));
+	}
+
+	// Getters and Setters
+
+	public int getRandomNumber() {
+		return randomNumber;
+	}
+
+	public void setRandomNumber(int randomNumber) {
+		this.randomNumber = randomNumber;
+	}
+
+	public int getMaxRange() {
+		return maxRange;
+	}
+
+	public void setMaxRange(int maxRange) {
+		this.maxRange = maxRange;
+	}
+
+	public int getMaxTries() {
+		return maxTries;
+	}
+
+	public void setMaxTries(int maxTries) {
+		this.maxTries = maxTries;
+	}
+
+	public int getTries() {
+		return tries;
+	}
+
+	public void setTries(int tries) {
+		this.tries = tries;
+	}
+
+	// Methods
+
+	// This method generates a random number
+	public int createRandomNumber(int randomNumber) {
+		Random random = new Random();
+		randomNumber = random.nextInt(maxRange);
+		return randomNumber;
+	}
+
+	// This method starts the game
+	public void startGame() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Bem-vindo ao jogo de adivinhação!");
+		System.out.println();
+		System.out.println("Tente adivinhar um número entre 1 e 100.");
+		System.out.println("Você tem " + getMaxTries() + " tentativas.");
+		System.out.println();
+		while (tries < getMaxTries()) {
+			tries++;
+			System.out.print("Digite o seu palpite: ");
+			int guess = sc.nextInt();
+			if (testGuess(guess)) {
+				break;
+			} /*
+				 * else { System.out.println("Você perdeu! O número era: " + randomNumber +
+				 * "."); }
+				 */
+
+		}
+		sc.close();
+	}
+
+	// This method tests the player's guess
+	public boolean testGuess(int guess) {
+		if (guess == randomNumber) {
+			System.out.println("Parabéns! Você acertou o número em " + tries + " tentativas.");
+			return true;
+		} else if (guess < randomNumber) {
+			System.out.println("O número é maior do que " + guess + ".");
+			System.out.println();
+		} else {
+			System.out.println("O número é menor do que " + guess + ".");
+			System.out.println();
+
+		}
+		return false;
+	}
+
 }
