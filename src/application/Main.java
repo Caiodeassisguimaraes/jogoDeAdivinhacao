95a8056497c68a43f62a734dae44831339d5a7e8
Caio de Assis Guimarães <caiodeassisguimaraes@hotmail.com>
2023-05-28 15:55:56 -0300

    solution

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,12 @@
 package application;
 
-import java.util.Scanner;
-
 import service.GuessingGame;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 
 		GuessingGame game = new GuessingGame(101, 10);
 		game.startGame();
-
-		sc.close();
 
 	}
 }
